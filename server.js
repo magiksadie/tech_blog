0323d7b1fb24da75c598aac9962bfd526595e627
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-26 09:22:06 -0500

    set up initial server

---
+++ server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'keyboard cat',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(session(sess));
+app.use(express.static((path.join(__dirname, "public"))));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+        console.log(`Now listening on port ${PORT}`);
+    });
+});
