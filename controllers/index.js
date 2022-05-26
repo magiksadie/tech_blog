90e5f76295f0ab827efeac3d37534cf9036a3705
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-26 09:21:43 -0500

    created index routes in controllers

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home');
+const dashboardRoutes = require('./dashboard.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('404: Page not found');
+});
+
+module.export = router;
