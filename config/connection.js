6c9468a2b9a07b7a767b964609bd8459653d6c30
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-26 16:49:13 -0500

    ensure .env match

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = process.env.JAWSDB_URL
     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PASS,
+        process.env.DB_PW,
         {
             host: process.env.DB_HOST,
             dialect: 'mysql',
