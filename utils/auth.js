5a9653bf6d98c752cd6db165f10af4cbfcc02b1a
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-27 15:24:35 -0500

    simplified auth

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,9 @@
 const withAuth = (req, res, next) => {
-    if (!req.headers.authorization) {
-        return res.status(401).json({ message: 'No credentials sent!' });
+    if (!req.session.userId) {
+        res.redirect('/login');
+    } else {
+        next();
     }
-    const token = req.headers.authorization.split(' ')[1];
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = payload;
-    next();
 };
 
 module.exports = withAuth;
